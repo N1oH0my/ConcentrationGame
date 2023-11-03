30be72e759ddad3d93d7792a7c35cb73941f3eb3
N1oH0my <h0myn1o@mail.ru>
2023-11-03 00:46:34 -0700

    save settings data added

--- App/Models/GameSettings.swift
+++ App/Models/GameSettings.swift
@@ -38,12 +38,88 @@ enum DifficultyLevel {
 class GameSettings {
     static let shared = GameSettings()
     
-    var difficultyLevel: DifficultyLevel = .easy
+    var difficultyLevel: DifficultyLevel = .easy{
+        didSet {
+            saveDifficultyLevel()
+        }
+    }
     
-    var emojis = ["🍄", "🌿", "🤸🏻","🌱", "🎋", "🌵", "☘️", "🍀", "🗿","🕸", "🍁", "🌼"]
-    var backupEmojis = ["🍄", "🌿", "🤸🏻","🌱", "🎋", "🌵", "☘️", "🍀", "🗿","🕸", "🍁", "🌼"]
+    var emojis = ["🍄", "🌿", "🤸🏻","🌱", "🎋", "🌵", "☘️", "🍀", "🗿","🕸", "🍁", "🌼"]{
+        didSet {
+            saveEmojis()
+        }
+    }
+    var backupEmojis = ["🍄", "🌿", "🤸🏻","🌱", "🎋", "🌵", "☘️", "🍀", "🗿","🕸", "🍁", "🌼"]{
+        didSet {
+            saveBackupEmojis()
+        }
+    }
     
-    var backgroundColor: UIColor = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1) 
+    var backgroundColor: UIColor = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1) {
+        didSet {
+            saveBackgroundColor()
+        }
+    }
     
-    private init() {}
+    private init() {
+        loadDifficultyLevel()
+        loadEmojis()
+        loadBackupEmojis()
+        loadBackgroundColor()
+
+    }
+    
+    private let difficultyLevelDisplayNames: [DifficultyLevel: String] = [
+            .easy: "Easy",
+            .medium: "Medium",
+            .hard: "Hard"
+        ]
+        
+        var displayDifficultyLevel: String {
+            return difficultyLevelDisplayNames[difficultyLevel] ?? ""
+        }
+    private let userDefaults = UserDefaults.standard
+        
+    private func saveDifficultyLevel() {
+        let displayName = difficultyLevelDisplayNames[difficultyLevel]
+        userDefaults.set(displayName, forKey: "difficultyLevel")
+    }
+    
+    private func loadDifficultyLevel() {
+        if let displayName = userDefaults.string(forKey: "difficultyLevel"),
+           let level = difficultyLevelDisplayNames.first(where: { $0.value == displayName })?.key {
+            difficultyLevel = level
+        }
+    }
+    
+    private func saveEmojis() {
+        userDefaults.set(emojis, forKey: "emojis")
+    }
+    
+    private func loadEmojis() {
+        if let savedEmojis = userDefaults.object(forKey: "emojis") as? [String] {
+            emojis = savedEmojis
+        }
+    }
+    
+    private func saveBackupEmojis() {
+        userDefaults.set(backupEmojis, forKey: "backupEmojis")
+    }
+    
+    private func loadBackupEmojis() {
+        if let savedBackupEmojis = userDefaults.object(forKey: "backupEmojis") as? [String] {
+            backupEmojis = savedBackupEmojis
+        }
+    }
+    
+    private func saveBackgroundColor() {
+        userDefaults.set(NSKeyedArchiver.archivedData(withRootObject: backgroundColor), forKey: "backgroundColor")
+    }
+    
+    private func loadBackgroundColor() {
+        if let savedColorData = userDefaults.object(forKey: "backgroundColor") as? Data,
+           let color = NSKeyedUnarchiver.unarchiveObject(with: savedColorData) as? UIColor {
+            backgroundColor = color
+        }
+    }
 }
