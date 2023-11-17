4c83c0c00df1ff599714e91a800fbeb8bc8027da
N1oH0my <h0myn1o@mail.ru>
2023-11-16 23:19:45 -0800

    круг в центре

--- App/PlayingCardsView.swift
+++ App/PlayingCardsView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 class PlayingCardsView: UIView {
-    override func draw(_ rect: CGRect){
-        if let context = UIGraphicsGetCurrentContext(){
-            context.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.clear // 设置背景为透明
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        if let context = UIGraphicsGetCurrentContext() {
+            context.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY), radius: min(bounds.width, bounds.height) / 2.0, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+            context.setFillColor(UIColor.white.cgColor) 
+            context.fillPath()
         }
     }
+    
+    override func layoutSubviews() {
+            super.layoutSubviews()
+            
+            let diameter = min(bounds.width, bounds.height)
+            let radius = diameter / 2.0
+            
+            var newX = superview?.bounds.midX ?? 0
+            var newY = superview?.bounds.midY ?? 0
+            
+            newX = max(radius, min(newX, superview?.bounds.width ?? 0 - radius))
+            newY = max(radius, min(newY, superview?.bounds.height ?? 0 - radius))
+            
+            center = CGPoint(x: newX, y: newY)
+        }
 }
+
--- App/ViewController.swift
+++ App/ViewController.swift
@@ -9,114 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private lazy var game = Concentration(numberOfPairsOfCards: (cardButtons.count+1)/2) // пока ее не используют она не инициализируется
-    
-    private (set) var flipCount = 0 {
-        didSet{
-            updateFlipCountLabel()
-        }
-    }
-    
-    private func updateFlipCountLabel(){
-        let attributes: [NSAttributedString.Key: Any]=[
-            .strokeWidth: 5,
-            .strokeColor: UIColor.black]
-        let attributedString = NSAttributedString(string: "Flips:\(flipCount)", attributes: attributes)
-        flipCountLabel.attributedText = attributedString
-    }
-    
-    @IBOutlet var shuffleButton: UIButton!
-    
-    @IBOutlet private weak var flipCountLabel: UILabel!{
-        didSet{
-            updateFlipCountLabel()
-        }
-    }
-    
-    @IBOutlet private var cardButtons: [UIButton]!
-    
-    @IBAction private func touchSomeCard(_ sender: UIButton) {
-        flipCount += 1
-        if let cardNumber = cardButtons.firstIndex(of: sender) {
-            game.chooseCard(at: cardNumber)
-            updateViewFromModel()
-            print("cardNumber = \(cardNumber ?? -1)")
-            } else {
-                    print("Кнопка не найдена в массиве cardButtons")
-                }
-        
-    }
-    @IBAction private func shuffleCards() {
-        game.shuffleCards()
-        
-        updateViewFromModel()
-    }
-    private var emojis = "🍄🌿🤸🏻🌱🎋🌵☘️🍀🗿🕸🍁🌼"
-    //private var emojis = ["🍄", "🌿", "🤸🏻","🌱", "🎋", "🌵", "☘️", "🍀", "🗿","🕸", "🍁", "🌼"]
-    private var emoji = Dictionary<Int, String>()
-    private func getEmoji(for card: Card)-> String
-    {
-        if emoji[card.hashValue] == nil, emojis.count > 0{
-            let randomIndex = emojis.index( emojis.startIndex, offsetBy: emojis.count.arc4random)
-            emoji[card.hashValue] = String( emojis.remove(at: randomIndex))
-        }
-        
-        return emoji[card.hashValue] ?? "?"
-        
-    }
-    
-    private func updateViewFromModel(){
-        for index in cardButtons.indices {
-            let button = cardButtons[index]
-            let card = game.cards[index]
-            if card.isFaceUp && card.isMatched{
-                button.setTitle("", for: UIControl.State.normal)
-                button.backgroundColor = card.isMatched ? #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 0):#colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
-            }
-            else if card.isFaceUp {
-                button.setTitle(getEmoji(for: card), for: UIControl.State.normal)
-                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-            }
-            else{
-                button.setTitle("", for: UIControl.State.normal)
-                button.backgroundColor = card.isMatched ? #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 0):#colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
-        }
-    }
-    
-    func viewDidLoad() {
-            super.viewDidLoad()
-    }
-    
-    
-    func flipCard(withEmoji emoji: String, on button: UIButton) {
-        print("flipCard(withEmoji: \(emoji))")
-        if button.currentTitle == emoji {
-            button.setTitle("", for: UIControl.State.normal)
-            button.backgroundColor = #colorLiteral(red: 1, green: 0.5029296378, blue: 0.1438817539, alpha: 1)
-        }
-        else{
-            button.setTitle(emoji, for: UIControl.State.normal)
-            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        }
-    }
-    }
-    
-    
-    
-    
-    
     var deck = PlayingCardDeck()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        for _ in 1...10 {
-            if let card = deck.draw() {
-                print("\(card)")
+            
+            let playingCardsView = PlayingCardsView()
+            playingCardsView.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(playingCardsView)
+            
+            NSLayoutConstraint.activate([
+                playingCardsView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                playingCardsView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+                playingCardsView.widthAnchor.constraint(equalToConstant: 200),
+                playingCardsView.heightAnchor.constraint(equalToConstant: 200)
+            ])
+            
+            for _ in 1...10 {
+                if let card = deck.draw() {
+                    print("\(card)")
+                }
             }
-        }
     }
     
-    
 }
+    
 extension Int{
     var arc4random: Int{
         if self > 0 {
@@ -132,4 +49,5 @@ extension Int{
         
     }
 }
+
 
